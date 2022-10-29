62a78b85d50d006a24c35c0e5b6fda8af538c21a
LeonardoGonzlz <leonardodaniel2000gonzalez@gmail.com>
2022-10-29 12:49:53 -0400

    animaciones observer y slider agregadas

---
+++ javascript/script.js
@@ -0,0 +1,59 @@
+/* silider */
+(()=>{
+    const slider = document.querySelector("#slider");
+let projects = document.querySelectorAll(".project");
+
+const next = ()=>{
+    let firstProject = document.querySelectorAll(".project")[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 1s"
+
+    setTimeout(()=>{
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", firstProject)
+        
+        slider.style.marginLeft = "-100%";
+    }, 1000)
+
+}
+const prev = ()=>{
+    let lastProject = document.querySelectorAll(".project")[projects.length - 1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 1s"
+
+    setTimeout(()=>{
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", lastProject)
+        
+        slider.style.marginLeft = "-100%";
+    }, 1000)
+
+}
+
+document.addEventListener("click",(e)=>{
+    e = e.target.className
+    if(/button-left/.test(e)) prev();
+    if(/button-right/.test(e)) next();
+})
+
+setInterval(()=>{next()}, 13000)
+
+})();
+
+/* Observer */
+(()=>{
+    const $observer = document.querySelectorAll(".observer")
+    const cargarAnimcaionObserver = (entradas, observador)=>{
+    
+      entradas.forEach(element => {
+        if (element.isIntersecting)
+        element.target.classList.add("observer-animacion")
+      });
+    }
+    const observador = new IntersectionObserver(cargarAnimcaionObserver,{
+          rootMargin: "60px"
+    })
+    
+    $observer.forEach( el => observador.observe(el))
+    
+})();
